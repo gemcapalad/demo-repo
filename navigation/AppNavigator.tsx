b81305c51f20f86713b84670499a815f7b760dda
Lorenzo <74661418+winozz@users.noreply.github.com>
2024-12-03 01:27:42 +0800

    [base-app] fix navigator error

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from '../screens/HomeScreen';
 import DetailsScreen, { DetailsScreenProps } from '../screens/DetailsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator, StackScreenProps } from '@react-navigation/stack';
+import React from 'react';
+import HomeScreen from '../screens/HomeScreen'; // Ensure you have this import
 
-const Stack = createStackNavigator();
+type RootStackParamList = {
+  Home: undefined;
+  Details: DetailsScreenProps;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={(props: DetailsScreenProps) => <DetailsScreen {...props} route={props.route} />} />
+        <Stack.Screen name="Details" component={(DetailsScreen as unknown) as React.ComponentType<StackScreenProps<RootStackParamList, "Details">>} />
       </Stack.Navigator>
     </NavigationContainer>
   );
